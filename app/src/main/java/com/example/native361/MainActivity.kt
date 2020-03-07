5f938b79940d9ddd79b848507ad7e151644fe416
yono-ms <yono.ms@outlook.com>
2020-03-07 20:31:24 +0900

    first commit.

---
+++ app/src/main/java/com/example/native361/MainActivity.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020. No name
+ */
+
+package com.example.native361
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
