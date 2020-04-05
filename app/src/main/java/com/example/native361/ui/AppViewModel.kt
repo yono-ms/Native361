57263c3d213262a5a842f84c1fc18e463e4cc8bd
yono-ms <yono.ms@outlook.com>
2020-04-05 09:54:58 +0900

    busy (#8)

--- app/src/main/java/com/example/native361/ui/AppViewModel.kt
+++ app/src/main/java/com/example/native361/ui/AppViewModel.kt
@@ -6,7 +6,10 @@ package com.example.native361.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.native361.constant.Destination
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class AppViewModel : ViewModel() {
     /**
@@ -20,4 +23,22 @@ class AppViewModel : ViewModel() {
      * 遷移した瞬間にforegroundが非nullを受け取ることになるためfragmentで処理できない.
      */
     val destination: MutableLiveData<Destination> = MutableLiveData()
+
+    val isBusy: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    fun checkBusy(): Boolean {
+        return if (isBusy.value == true) {
+            true
+        } else {
+            isBusy.value = true
+            false
+        }
+    }
+
+    fun releaseBusy() {
+        viewModelScope.launch {
+            delay(200)
+            isBusy.postValue(false)
+        }
+    }
 }
